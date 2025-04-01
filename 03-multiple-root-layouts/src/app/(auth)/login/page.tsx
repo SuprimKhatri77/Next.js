b15b042e870dfc922f75f4e62ba1578df0e2c12e
SuprimKhatri77 <supremekhatri62@gmail.com>
2025-04-01 18:46:27 +0545

    multiple-root-layout

---
+++ 03-multiple-root-layouts/src/app/(auth)/login/page.tsx
@@ -0,0 +1,5 @@
+export default function Login(){
+    return (
+        <h1 className="h-screen flex justify-center items-center text-4xl font-bod">Login</h1>
+    )
+}
---
+++ 03-multiple-root-layouts/src/app/(auth)/register/page.tsx
@@ -0,0 +1,5 @@
+export default function Register(){
+    return (
+        <h1 className="h-screen flex justify-center items-center text-4xl font-bod">Register</h1>
+    )
+}
