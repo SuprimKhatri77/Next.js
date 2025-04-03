f8e6d7c21daa5aefc7311ee431494b102cb7a6d5
SuprimKhatri77 <supremekhatri62@gmail.com>
2025-04-03 19:56:58 +0545

    Intro to NextJS

---
+++ 01-nextjs/src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+    <h1 className="text-5xl font-bold text-center h-screen flex items-center justify-center">Hello world</h1>
+    </>
+  );
+}
