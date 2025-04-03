d0ff9e63df7cf205b206e7bf0b72bbaaacb39145
SuprimKhatri77 <supremekhatri62@gmail.com>
2025-04-03 19:48:51 +0545

    different requests, query params and headers

---
+++ 04-route-handlers/src/app/api/profile/route.ts
@@ -0,0 +1,17 @@
+import { type NextRequest } from "next/server"
+import { headers } from "next/headers";
+
+export async function GET(request: NextRequest){
+    // const requestHeaders = new Headers(request.headers)
+    // console.log(requestHeaders.get('Authorization'));
+
+    const headersList = await headers()
+    console.log(headersList.get("Authorization"));
+    
+    
+    return new Response("<h1>Profile API data slashyy.</h1>", {
+        headers: {
+            "Content-Type": "text/html"
+        }
+    })
+}
